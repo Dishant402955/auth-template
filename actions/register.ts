0e885c83a1e3d120d9ab3a81c55d1a15bc816481
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-05-19 22:02:20 +0530

    Connect Register form with DB

--- actions/register.ts
+++ actions/register.ts
@@ -2,15 +2,39 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db, users } from "@/lib/db";
+import { eq } from "drizzle-orm";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-	console.log(values);
-
 	const validatedFields = RegisterSchema.safeParse(values);
 
 	if (!validatedFields.success) {
 		return { error: "Invalid Fields!" };
 	}
+
+	const { email, name, password } = validatedFields.data;
+
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const existingUser = await db
+		.select()
+		.from(users)
+		.where(eq(users.email, email));
+
+	if (existingUser[0]) {
+		console.log(existingUser);
+		return { error: "Email is already taken" };
+	}
+
+	const user: typeof users.$inferInsert = {
+		name,
+		email,
+		password: hashedPassword,
+	};
+
+	const res = await db.insert(users).values(user);
+	console.log("New user created!", res);
 
 	return { success: "Email Sent!" };
 };
--- lib/db.ts
+++ lib/db.ts
@@ -2,27 +2,67 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/neon-http";
 
-const db = drizzle(process.env.DB_URI!);
+import {
+	boolean,
+	timestamp,
+	pgTable,
+	text,
+	primaryKey,
+	integer,
+	varchar,
+} from "drizzle-orm/pg-core";
 
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import type { AdapterAccountType } from "next-auth/adapters";
 
-export const usersTable = pgTable("users", {
-	id: integer().primaryKey().generatedAlwaysAsIdentity(),
-	name: varchar({ length: 255 }).notNull(),
-	email: varchar({ length: 255 }).notNull().unique(),
-	password: varchar({ length: 255 }),
+export const db = drizzle(process.env.DB_URI!);
+
+export const users = pgTable("user", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	name: text("name"),
+	email: text("email").unique(),
+	emailVerified: timestamp("emailVerified", { mode: "date" }),
+	image: text("image"),
+	password: text("password"),
 });
 
-async function main() {
-	const user: typeof usersTable.$inferInsert = {
-		name: "John",
-		email: "john@example.com",
-		password: "123456",
-	};
+export const accounts = pgTable(
+	"account",
+	{
+		userId: text("userId")
+			.notNull()
+			.references(() => users.id, { onDelete: "cascade" }),
+		type: text("type").$type<AdapterAccountType>().notNull(),
+		provider: text("provider").notNull(),
+		providerAccountId: text("providerAccountId").notNull(),
+		refresh_token: text("refresh_token"),
+		access_token: text("access_token"),
+		expires_at: integer("expires_at"),
+		token_type: text("token_type"),
+		scope: text("scope"),
+		id_token: text("id_token"),
+		session_state: text("session_state"),
+	},
+	(account) => [
+		{
+			compoundKey: primaryKey({
+				columns: [account.provider, account.providerAccountId],
+			}),
+		},
+	]
+);
 
-	await db.insert(usersTable).values(user);
-	console.log("New user created!");
+// async function main() {
+// 	const user: typeof usersTable.$inferInsert = {
+// 		name: "John",
+// 		email: "john@example.com",
+// 		password: "123456",
+// 	};
 
-	const users = await db.select().from(usersTable);
-	console.log("Getting all users from the database: ", users);
-}
+// 	await db.insert(usersTable).values(user);
+// 	console.log("New user created!");
+
+// 	const users = await db.select().from(usersTable);
+// 	console.log("Getting all users from the database: ", users);
+// }
