3b0ca994668cf0c911f1a761795a0254fcf169b7
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-05-19 19:22:35 +0530

    Mock Server Action for Register

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,6 +1,55 @@
-import { CardWrapper } from "./card-wrapper";
+"use client";
+
+import * as z from "zod";
+
+import { useState, useTransition } from "react";
+import { RegisterSchema } from "@/schemas";
+
+import { useForm } from "react-hook-form";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { register } from "@/actions/register";
 
 export const RegisterForm = () => {
+	const [isPending, startTransition] = useTransition();
+
+	const [error, setError] = useState<string | undefined>("");
+	const [success, setSuccess] = useState<string | undefined>("");
+
+	const form = useForm<z.infer<typeof RegisterSchema>>({
+		resolver: zodResolver(RegisterSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+			name: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+		startTransition(() => {
+			setError("");
+			setSuccess("");
+
+			register(values).then((data) => {
+				setError(data.error);
+				setSuccess(data.success);
+			});
+		});
+	};
+
 	return (
 		<CardWrapper
 			headerLabel="Welcome Back"
@@ -8,7 +57,79 @@ export const RegisterForm = () => {
 			backButtonLabel="Already Have an Account ?"
 			showSocial={true}
 		>
-			<h1>Register Form</h1>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="space-y-3 w-[90%]"
+				>
+					<div className="space-y-4">
+						<FormField
+							control={form.control}
+							name="name"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											placeholder="John Doe"
+											className="w-full"
+											type="text"
+											disabled={isPending}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={form.control}
+							name="email"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											placeholder="john@doe.com"
+											className="w-full"
+											type="email"
+											disabled={isPending}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											placeholder="******"
+											className="w-full"
+											type="password"
+											disabled={isPending}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<FormError message={error} />
+					<FormSuccess message={success} />
+					<Button type="submit" className="w-full" disabled={isPending}>
+						Login
+					</Button>
+				</form>
+			</Form>
 		</CardWrapper>
 	);
 };
--- schemas/index.ts
+++ schemas/index.ts
@@ -3,13 +3,16 @@ import * as z from "zod";
 export const LoginSchema = z.object({
 	email: z.string().email(),
 	password: z.string().min(1, {
-		message: "passwoord is required",
+		message: "password is required",
 	}),
 });
 
 export const RegisterSchema = z.object({
 	email: z.string().email(),
-	username: z.string().max(20),
-	password: z.string().min(6),
-	confirmPassword: z.string().min(6),
+	password: z.string().min(6, {
+		message: "Minimum 6 characters required",
+	}),
+	name: z.string().min(1, {
+		message: "name is required",
+	}),
 });
